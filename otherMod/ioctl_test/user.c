64e64838fcfdb98bfddf87558940d61515fc2a6d
confidentoh <confidentoh@foxmail.com>
2022-04-23 17:55:26 +0800

    hhh

--- otherMod/ioctl_test/user.c
+++ otherMod/ioctl_test/user.c
@@ -13,6 +13,8 @@
 /* Functions for the ioctl calls */ 
 
 pid_t p_work;
+
+char message[100] = { 0 };
  
 int ioctl_set_msg(int file_desc, char *message) 
 { 
@@ -30,8 +32,6 @@ int ioctl_set_msg(int file_desc, char *message)
 int ioctl_get_msg(int file_desc) 
 { 
     int ret; 
-    char message[100] = { 0 }; 
- 
     /* Warning - this is dangerous because we don't tell  
    * the kernel how far it's allowed to write, so it  
    * might overflow the buffer. In a real production  
@@ -44,7 +44,7 @@ int ioctl_get_msg(int file_desc)
     if (ret < 0) { 
         printf("ioctl_get_msg failed:%d\n", ret); 
     } 
-    printf("get_msg message:%s", message); 
+    printf("get_msg message:%s\n", message); 
  
     return ret; 
 } 
@@ -71,7 +71,7 @@ int ioctl_get_nth_byte(int file_desc)
 /* Main - Call the ioctl functions */ 
 int main(void) 
 { 
-    int file_desc, ret, status; 
+    int file_desc, ret, status, len; 
 
     file_desc = open(DEVICE_PATH, O_RDWR); 
     if (file_desc < 0) { 
@@ -81,19 +81,27 @@ int main(void)
     } 
 
     for (; ; ) {
-        p_work = fork();
-        if (p_work == 0) {
-            close(file_desc); 
-            int code = system("sudo ls &");
-            printf("system end\n");
-            exit(0);
+        ret = ioctl_get_msg(file_desc); 
+        if (ret) {
+            goto error; 
         }
-        else {
-            ret = ioctl_get_msg(file_desc); 
-            if (ret) 
-                goto error; 
-            ret = waitpid(p_work, &status, 0);
+        len = strlen(message);
+        if (len > 0 && message[len - 1] != 1) {
+            printf("%s\n", message);
+            message[len +1] = '\0';
+            message[len] = 1;
+            p_work = fork();
+            if (p_work == 0) {
+                close(file_desc); 
+                int code = system("sudo ls &");
+                printf("system end\n");
+                exit(0);
+            }
+            else {
+                ret = waitpid(p_work, &status, 0);
+            }
         }
+        
     }
 
 loop_out:
